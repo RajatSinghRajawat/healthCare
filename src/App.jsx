dff61045d3dab2af782f5f10a6937e06d2e151eb
Rajat Singh Rajawat <rajatsinghrajawatrajawat@gmail.com>
2025-08-20 12:52:56 +0530

    sd

--- src/App.jsx
+++ src/App.jsx
@@ -1,103 +1,11 @@
-// import React, { useState, useEffect } from "react";
-// import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-// import ThemeContext from "./ThemeContext.jsx";
-// import Navbar from "./Components/Navbar";
+import React from 'react'
 
-// function App() {
-//   const [isDarkMode, setIsDarkMode] = useState(false);
-
-//   // Add/remove dark mode class from HTML
-//   useEffect(() => {
-//     document.documentElement.classList.toggle("dark", isDarkMode);
-//   }, [isDarkMode]);
-
-//   const theme = {
-//     isDarkMode,
-//     toggleTheme: () => setIsDarkMode((prev) => !prev),
-//     colors: {
-//       primary: "#6924b9", // new yellow
-//       lightBg: "#fff",
-//       darkBg: "#000",
-//       lightText: "#000",
-//       darkText: "#fff",
-//     },
-//   };
-
-//   return (
-//     <ThemeContext.Provider value={theme}>
-//       <Router>
-//         <div
-//           style={{
-//             backgroundColor: isDarkMode
-//               ? theme.colors.darkBg
-//               : theme.colors.lightBg,
-//             color: isDarkMode ? theme.colors.darkText : theme.colors.lightText,
-//             minHeight: "100vh",
-//           }}
-//         >
-//           <Navbar />
-//           <Routes>
-//             {/* <Route path="/" element={<Home />} /> */}
-//           </Routes>
-//         </div>
-//       </Router>
-//     </ThemeContext.Provider>
-//   );
-// }
-
-// export default App;
-
-import React, { useState, useEffect } from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import ThemeContext from "./ThemeContext.jsx";
-import Navbar from "./Components/Navbar";
-import Footer from "./Components/Footer";
-import Contact from "./Pages/Contact_Support.jsx";
-function App() {
-  const [isDarkMode, setIsDarkMode] = useState(false);
-
-  // Add/remove dark mode class from HTML
-  useEffect(() => {
-    document.documentElement.classList.toggle("dark", isDarkMode);
-  }, [isDarkMode]);
-
-  const theme = {
-    isDarkMode,
-    toggleTheme: () => setIsDarkMode((prev) => !prev),
-    colors: {
-      primary: "#6924b9", // bg-yellow-500
-      lightBg: "#fff",
-      darkBg: "#000",
-      lightText: "#000",
-      darkText: "#fff",
-    },
-  };
-
+const App = () => {
   return (
-    <ThemeContext.Provider value={theme}>
-      <Router>
-        <div
-          style={{
-            backgroundColor: isDarkMode
-              ? theme.colors.darkBg
-              : theme.colors.lightBg,
-            color: isDarkMode ? theme.colors.darkText : theme.colors.lightText,
-            minHeight: "100vh",
-            display: "flex",
-            flexDirection: "column",
-          }}
-        >
-          <Navbar />
-          <main className="flex-grow">
-            <Routes>
-              <Route path="/contact-support" element={<Contact />} />
-            </Routes>
-          </main>
-          <Footer />
-        </div>
-      </Router>
-    </ThemeContext.Provider>
-  );
+    <div className='h-45 w-50 bg-black'>
+     
+    </div>
+  )
 }
 
-export default App;
+export default App
